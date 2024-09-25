f51e03a7bb46de636aa9e8b6546eac65c027beff
Fatema-tuj-Jahura <fatema.tuj.jahura@g.bracu.ac.bd>
2024-09-25 08:39:01 +0600

    changed

--- blog.js
+++ blog.js
@@ -1,4 +1,2 @@
 document.getElementById('home')
-         .addEventListener('click', function(){
-           window.location.href = '/index.html';
-        })
+         .addEventListener('click', function() { window. location. href = 'https://fatema-tuj-jahura.github.io/flood-donation-website/' });
